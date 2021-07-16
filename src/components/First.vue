68ce2806ee8d191573c0f16fb8aff79c2bf907e6
Lucas Passos <lucas.depassos@yahoo.com.br>
2021-07-16 18:28:39 -0300

    make it better

--- src/components/First.vue
+++ src/components/First.vue
@@ -2,6 +2,7 @@
    
     <v-container style="width:1500px;height:700px;display:flex;justify-content:center;align-items:center;flex-direction:column;">
     <v-img style="max-width:520px;max-height:320px;" src="../assets/frottas.png" />
+    a
     <v-img id="mouse1" style="max-width:120px;max-height:160px;" src="../assets/mouse.png" />
     </v-container>
     
